6dd1c4d06d7f7cc92cb1c91a1455c2326f162e44
RahulKandagatla <47211010+RahulKandagatla@users.noreply.github.com>
2019-10-15 16:08:48 +0530

    first commit

---
+++ src/main/java/com/app/UsefulApiApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsefulApiApplication.class, args);
+	}
+
+}
